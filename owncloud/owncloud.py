be4045ddaa79adc3a5e281e34d00555643bafd3d
lzb13612 <1072095696@qq.com>
2023-08-11 11:34:22 +0800

    perf: add mkdir folder exist check

--- owncloud/owncloud.py
+++ owncloud/owncloud.py
@@ -718,6 +718,24 @@ class Client(object):
         """
         if not path.endswith('/'):
             path += '/'
+
+        path_split = list(filter(None, path.split("/")))
+        try:
+            if len(path_split) > 1:
+                file_list = [x.get_name() for x in self.list(self._normalize_path("/".join(path_split[0:-1])))]
+
+                if path_split[-1] in file_list:
+                    raise FileExistsError(f"Remote folder {path_split[-1]} exist, please check!")
+            else:
+                file_list = [x.get_name() for x in self.list(path="./")]
+                if path_split[0] in file_list:
+                    raise FileExistsError(f"Remote folder {path_split[-1]} exist, please check!")
+        except HTTPResponseError as e:
+            if e.status_code == 404:
+                raise FileExistsError(f"Remote folder {path_split[-1]} exist, please check!")
+            else:
+                raise e
+
         return self._make_dav_request('MKCOL', path)
 
     def delete(self, path):
